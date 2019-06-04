11a51947f6fbed73125bf889e480b5c3bce963ef
danialbleile <danial.bleile@wsu.edu>
2019-06-04 10:16:23 -0700

    bump version 1.2.5

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WSUWP Plugin Spine Themes
-Version: 1.2.3
+Version: 1.2.5
 Description: Plugin to add additional Spine themes.
 Author: washingtonstateuniversity, Danial Bleile
 Author URI: https://web.wsu.edu/
